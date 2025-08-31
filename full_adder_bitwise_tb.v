c65b5ae027196339b689f8ad7c77fed77b1e1a98
Muttireddy-Vasanth <vasanth.muttireddy@gmail.com>
2025-08-31 23:43:04 +0530

    Create full_adder_bitwise_tb.v

---
+++ full_adder_bitwise_tb.v
@@ -0,0 +1,37 @@
+// Author: Vasanth, 2022
+module full_adder_bitwise_tb();
+  // Testbench global variables
+  reg a, b, cin;
+  wire sum, cout;
+
+  // Variable for loop iteration
+  integer i;
+
+  // Instantiate the full adder with ordered port mapping
+  full_adder_bitwise dut(a, b, cin, sum, cout);
+
+  // Initialize the variables at 0s
+  initial begin
+    {a, b, cin} = 3'b000;
+  end
+
+  // Generate stimulus using for loop
+  initial begin
+    for (i = 0; i < 8; i = i + 1) begin
+      {a, b, cin} = i;
+      #10;
+    end
+  end
+
+  // Monitor the changes in the variables
+  initial begin
+    $monitor("inputs a = %b, b = %b, c = %b, outputs Sum = %b, carry = %b", a, b, cin, sum, cout);
+  end
+
+  // Terminate simulation after 100ns
+  initial begin
+    #100;
+    $finish();
+  end
+
+endmodule
