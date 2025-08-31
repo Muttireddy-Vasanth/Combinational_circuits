911743fea52b4e220ee02caf7075af6804133a77
Muttireddy-Vasanth <vasanth.muttireddy@gmail.com>
2025-08-31 23:44:03 +0530

    Create decoder.v

---
+++ decoder.v
@@ -0,0 +1,11 @@
+// Author: Vasanth, 2022
+module decoder(
+  input a, b, en,
+  output d0, d1, d2, d3
+);
+  // Continuous assignment of decoder outputs
+  assign d0 = (~a) & (~b) & en;
+  assign d1 = (~a) & b & en;
+  assign d2 = a & (~b) & en;
+  assign d3 = a & b & en;
+endmodule
